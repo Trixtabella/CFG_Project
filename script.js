d263bf74df78680d6c8ac7c98932eef7d6ea3e81
Trixtabella <119008757+Trixtabella@users.noreply.github.com>
2023-08-07 17:14:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,15 +21,48 @@ const quotesWithImages = [
     },
     {
       quote: "Romance Quote 1",
-      image: "romance_image_url_1.jpg",
+      image: "C:\Users\Admin\Documents\GitHub\CFG_Project\img\Adalyn Grace, Belladonna.png",
       genre: "romance",
     },
     {
       quote: "Romance Quote 2",
-      image: "romance_image_url_2.jpg",
+      image: "C:\Users\Admin\Documents\GitHub\CFG_Project\img\Ali Hazelwood Love Hypothisis1.png",
       genre: "romance",
     },
-    // Add more objects with quotes, image URLs, and genres as needed
+    {
+        quote: "Romance Quote 3",
+        image: "C:\Users\Admin\Documents\GitHub\CFG_Project\img\Ali Hazelwood, The Love Hypothesis2.png",
+        genre: "romance",
+      },
+      {
+        quote: "Romance Quote 4",
+        image: "C:\Users\Admin\Documents\GitHub\CFG_Project\img\Casey McQuiston, Red, White and Royal Blue.png",
+        genre: "romance",
+      },{
+        quote: "Romance Quote 5",
+        image: "C:\Users\Admin\Documents\GitHub\CFG_Project\img\Jude Deveraux, A Knight in Shining Armor.png",
+        genre: "romance",
+      },{
+        quote: "Romance Quote 6",
+        image: "C:\Users\Admin\Documents\GitHub\CFG_Project\img\Kelly Andrew, The Whispering Dark.png",
+        genre: "romance",
+      },{
+        quote: "Romance Quote 7",
+        image: "C:\Users\Admin\Documents\GitHub\CFG_Project\img\Nicholas Sparks, Message in a Bottle.png",
+        genre: "romance",
+      },{
+        quote: "Romance Quote 8",
+        image: "C:\Users\Admin\Documents\GitHub\CFG_Project\img\Sarah Mayberry, Her Best Friend.png",
+        genre: "romance",
+      },{
+        quote: "Romance Quote 9",
+        image: "C:\Users\Admin\Documents\GitHub\CFG_Project\img\V.E. Schwab, The Invisible Life of Addie LaRue.png",
+        genre: "romance",
+      },{
+        quote: "Romance Quote 10",
+        image: "C:\Users\Admin\Documents\GitHub\CFG_Project\img\William Shakespear Romeo&Julitte.png",
+        genre: "romance",
+      },
   ];
 
   function getRandomImageByGenre(genre) {
